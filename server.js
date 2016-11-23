32260df9cfb6352ec125711a3a49cef4581b2922
salihigde <salihigde@gmail.com>
2016-11-23 23:33:01 +0300

    yazı düzenlendi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, resp) {
-    resp.send("Benim Web Sitem");
+    resp.send("<h2>Web Sitem</h2>");
 });
 
 app.listen(8080);
