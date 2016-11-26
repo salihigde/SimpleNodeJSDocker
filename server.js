8171c53ecc842677d36b31e15bb3d6372459b948
salihigde <salihigde@gmail.com>
2016-11-27 02:32:36 +0300

    html düzenleme

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, resp) {
-    resp.send("<h2>Web Sitem</h2>");
+    resp.send("<h2>Benim Web Siteme Hoş Geldiniz! :)</h2>");
 });
 
 app.listen(8080);
